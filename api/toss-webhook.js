1620413f7655eef7d16fd4815ac7e6031624611a
WSUNOH <wsunoh@gmail.com>
2025-08-04 15:27:11 +0900

    Update toss-webhook.js

--- api/toss-webhook.js
+++ api/toss-webhook.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
 
   const {
-    name, birth, plan, orderId, amount, discount, method, product
+    user_id, name, birth, orderId, amount, discount, method, product
   } = req.body;
 
   const auth = new JWT({
@@ -19,11 +19,19 @@ export default async function handler(req, res) {
   const spreadsheetId = process.env.SHEET_ID;
   const sheetName = '시트1'; // 원하는 시트 이름
 
-  const now = new Date().toISOString();
+  const now = new Date().toLocaleString('ko-KR', {
+  timeZone: 'Asia/Seoul',
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit'
+});
 
   await sheets.spreadsheets.values.append({
     spreadsheetId,
-    range: `${sheetname}!A1`,
+    range: `${sheetName}!A1`,
     valueInputOption: 'USER_ENTERED',
     requestBody: {
       values: [[user_id, name, birth, orderId, amount, discount, method, product, now]],
